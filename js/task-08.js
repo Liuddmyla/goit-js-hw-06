a60169ba4f7665a7300de2a935bd74827b3461a9
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-01-12 15:26:52 +0200

    9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
 
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+
+    event.preventDefault();
+   
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+
+        return alert("Увага! Всі поля повинні бути заповнені!");
+    }; 
+    
+    const dataName = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(dataName);    
+
+    event.currentTarget.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+btnEl.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  spanEl.textContent = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  
 }
